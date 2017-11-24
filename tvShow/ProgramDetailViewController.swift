c0f6c5343f8f4c31454f96bc9955b3c0af22b8a1
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2017-11-24 11:35:34 -0300

    Change: updated detail view controler

--- tvShow/ProgramDetailViewController.swift
+++ tvShow/ProgramDetailViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ProgramDetailViewController: UIViewController {
 
+	@IBOutlet weak var image: UIImageView!
+
 	var program : TVShow!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 
 		self.title = program.name
+		if let imageURL = program.posterPath{
+			image.imageURL(MoviesService().imageURL(path: imageURL), placeholder: #imageLiteral(resourceName: "moviePlaceholder"))
+		}else{
+			image.image = #imageLiteral(resourceName: "moviePlaceholder")
+		}
+		
 		
         // Do any additional setup after loading the view.
     }
